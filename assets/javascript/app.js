cfdb2ad2856cafb5b3b5c1ead58e3ebc09b6c9b3
Karina Solorio <macuser@Karinas-MacBook-Pro.local>
2018-04-12 07:56:49 -0400

    added audio, and 3 different screens

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,26 @@
 $(document).ready(function(){
-   
-    $('#timer').text('0');
+   //Hide questions when page is loaded 
+    $('.page-1').hide();
     $('.main-section').show();
+    $('#countdown').show();
+    $('.result').hide();
+
+
+    //play audio when start button is clicked
+    var audio = new Audio('./assets/audio/SuperMarioBros.mp3');
+    var loserAudio = new Audio('./assets/audio/loser.mp3');
+    var winnerAudio = new Audio('./assets/audio/winner.mp3');
+    
+    //when start button is cliked 
+    $('#start-button').click(function() {
+      audio.play();
+      $('.main-section').show();
+      $('.page-1').hide();
+      $('#countdown').show();
+      $('.result').hide();
+    }); 
+
+    
+
+
   });
