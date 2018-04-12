d7ab9f411413ef2c787143f33b564ccea2f0450f
Karina Solorio <macuser@Jennifers-Air-2.localdomain>
2018-04-12 14:27:18 -0400

    added timers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,16 @@
 $(document).ready(function(){
-   //Hide questions when page is loaded 
-    $('.page-1').hide();
+   //Hide questions, timer, and results when page is loaded 
+    $('.page-1').show();
     $('.main-section').hide();
     $('#countdown').hide();
-    $('.result').show();
+    $('.result').hide();
 
 
-    //play audio when start button is clicked
+    //audio files for trivia
     var audio = new Audio('./assets/audio/SuperMarioBros.mp3');
     var loserAudio = new Audio('./assets/audio/loser.mp3');
     var winnerAudio = new Audio('./assets/audio/winner.mp3');
+    var hurryAudio = new Audio('./assets/audio/hurr.mp3');
     
     //when start button is cliked 
     $('#start-button').click(function() {
@@ -20,7 +21,14 @@ $(document).ready(function(){
       $('.result').hide();
     }); 
 
-    
-
-
+    //timer funciton
+    var timeLeft = 60;
+    var timer = setInterval(function() {
+      document.getElementById('#timer').value = 60 - --timeLeft;
+        if (timeLeft <= 0)
+          clearIntervla(timer);
+    }, 10000);
+      
+    timer; 
+     
   });
